8d88cd60ffcb95b202017b4e7bfe73ec2477854b
narusawakohei <swkei320@gmail.com>
2024-02-14 21:55:04 +0900

    wrench: 🔧 設定見直し

--- packages/shared-components/webpack.config.js
+++ packages/shared-components/webpack.config.js
@@ -1,31 +1,31 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
+  mode: "development",
+  entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript'
-            ]
-          }
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
         },
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'], // 解決するファイルの拡張子
+    extensions: [".tsx", ".ts", ".js"], // 解決するファイルの拡張子
   },
 };
